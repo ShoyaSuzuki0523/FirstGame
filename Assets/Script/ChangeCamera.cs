1101e7a6c387de08348e1cfd6823f629dae2c713
ShoyaSuzuki0523 <bellwood0523@gmail.com>
2020-03-14 16:52:29 +0900

    first commit

---
+++ Assets/Script/ChangeCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+  public Camera Camera;
+
+  void OnTriggerEnter(Collider other){
+    if(other.CompareTag("Player")){
+      GameObject[] otherCameras = GameObject.FindGameObjectsWithTag("GameCamera");
+      foreach(GameObject camera in otherCameras){
+        camera.GetComponent<Camera>().enabled = false;
+      }
+      Camera.enabled = true;
+    }
+  }
+
+  void OnTriggerStay(Collider other){
+    Camera.enabled = true;
+  }
+
+  void OnTriggerExit(Collider other){
+    if(other.CompareTag("Player")){
+      Camera.enabled = false;
+    }
+  }
+}
