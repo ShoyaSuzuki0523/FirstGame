8d195d38c36ea36c0c599988a8a97614db6d3fae
shoya0523 <bellwood0523@gmail.com>
2020-03-25 19:59:53 +0900

    学校1会

---
+++ Assets/Script/ChangeCamera1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+  void OnTriggerStay(Collider other){
+    if(other.CompareTag("Player")){
+      Camera.enabled = true;
+    }
+  }
+}
