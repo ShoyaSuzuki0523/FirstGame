ff078c83a9ddcc4598de5d99cd94a5986be1a939
shoya0523 <bellwood0523@gmail.com>
2020-04-02 01:06:02 +0900

    ドア開閉機能の追加

---
+++ Assets/Script/DoorOpener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    private bool isNear;
+    private Animator animator;
+    public GameObject go;
+
+    void Start()
+    {
+        isNear = false;
+        animator = go.GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("space") && isNear) {
+            animator.SetBool("OPEN", !animator.GetBool("OPEN"));
+        }
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.tag == "Player") {
+            isNear = true;
+        }
+    }
+ 
+    void OnTriggerExit(Collider col) {
+        if (col.tag == "Player") {
+            isNear = false;
+        }
+    }
+}
